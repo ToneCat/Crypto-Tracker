b9a6b98bf5a16c1c06eb56d17f06535921eeb7b2
Tony Catalfamo <tony@Tonys-MacBook-Pro.local>
2019-10-10 17:34:25 -0400

    added styling, more table fixes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import MUIDataTable from "mui-datatables";
 import './App.css';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
 
 class App extends Component {
+ 
 
   // set the state (here we used states to ensure the API call was finished before rendering the table)
   state = {
@@ -12,25 +14,61 @@ class App extends Component {
   }
 
   pricesone = []; //setting array so we can fill it with JSON data from API Call
- 
- 
+
   
   componentDidMount() {
     fetch('http://localhost:3001/prices') // fetches the data from the go server
     .then(res => res.json())
     .then((res) => {
       for(var i in res) // converts JSON to array so it can be parsed and inserted with MUIDataTable
-    this.pricesone.push([i, res [i].USD])
+    this.pricesone.push([i, res[i].USD])
     this.setState({ prices: this.pricesone }) // sets current state to the array
-    
-      
-
     })
     .catch(console.log)
   }
 
   render(){
-  const columns = ["Currency", "Price"]; // sets columns
+
+    const columns = [
+  
+      {
+        name: "Currency",
+        options: {
+          filter: true,
+        }
+      },
+      {
+        label: "Price (USD)",
+        options: {
+          filter: true,
+        }
+      },
+      {
+        name: "More Info",
+        options: {
+          filter: false,
+          sort: false,
+          empty: true,
+          customBodyRender: (value, tableMeta, updateValue) => {
+        
+            return (
+              <button onClick={() => 
+            //  window.alert(`Clicked "Edit" for row ${tableMeta.rowIndex}`)
+            window.open("https://cryptowat.ch/assets/"+tableMeta.rowData[0])
+            
+
+              }>
+             
+           
+              <img id="chart" src="https://img.icons8.com/nolan/64/000000/positive-dynamic.png"></img>
+            
+              </button>
+            );
+          }
+        }
+        
+      }
+    ];
 
   const options = {
     print: false,
@@ -40,22 +78,24 @@ class App extends Component {
     selectableRows: false
   };
 
-  return(
+  
  
 
-
-
+  return(
+<div id="header">Hello</div>,
+    <div id="table">
   <MUIDataTable 
     title={"Cryptocurrency Prices"} 
     data={this.state.prices} 
     columns={columns}
     options={options}/>
+    </div>
+  
   
 )
 }
-  
- 
-  
+
 }
+
 export default App;
 render(<App />, document.getElementById('root'));
